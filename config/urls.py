21722e49c7798eb256dfe07f821102d91f3c93a9
Abdulhamid <73017095+Abdulhamid51@users.noreply.github.com>
2022-03-31 21:18:50 +0500

    modified:   .gitignore 	modified:   config/settings.py 	modified:   config/urls.py 	cart/migrations/0003_cart_price_alter_cart_quantity_order_maincart.py

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from . import settings
 
 urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),
+    # path('__debug__/', include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('cart/', include('cart.urls', namespace='cart')),
